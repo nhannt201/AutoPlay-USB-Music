1509eec6b86361c6bd91ca32472311095396a11c
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2021-11-13 09:15:36 +0700

    Add files via upload

---
+++ AutoPlay USB Music/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tm1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.paths = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.tm2 = New System.Windows.Forms.Timer(Me.components)
+        Me.tm3 = New System.Windows.Forms.Timer(Me.components)
+        Me.tm4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.andi = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tm1
+        '
+        Me.tm1.Interval = 1000
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 43)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(207, 121)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "My USB Music"
+        '
+        'paths
+        '
+        Me.paths.AutoSize = True
+        Me.paths.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.paths.Location = New System.Drawing.Point(348, 18)
+        Me.paths.Name = "paths"
+        Me.paths.Size = New System.Drawing.Size(0, 25)
+        Me.paths.TabIndex = 2
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(263, 88)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(263, 199)
+        Me.ListBox2.TabIndex = 3
+        '
+        'tm2
+        '
+        Me.tm2.Enabled = True
+        Me.tm2.Interval = 1000
+        '
+        'tm3
+        '
+        Me.tm3.Interval = 500
+        '
+        'tm4
+        '
+        Me.tm4.Interval = 1500
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(350, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Find List Music...."
+        '
+        'andi
+        '
+        Me.andi.Interval = 1500
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 227)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 60)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "YES"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Stop Play Music in USB?"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(400, 301)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "0"
+        Me.Label4.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(134, 227)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 60)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "NO"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(547, 339)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.paths)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "home"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AutoPlay USB Music"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tm1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents paths As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents tm2 As Timer
+    Friend WithEvents tm3 As Timer
+    Friend WithEvents tm4 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents andi As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ AutoPlay USB Music/Form1.vb
@@ -0,0 +1,187 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+
+Public Class home
+
+    Dim myd As DriveInfo
+    Dim chk As String = "b"
+    Dim keyboard_used As Boolean = False
+    Dim mouse_used As Boolean = False
+    Private Declare Function BlockInput Lib "user32" (ByVal fBlock As Long) As Long
+    Dim localByName As Process() = Process.GetProcessesByName("Music.UI")
+    Dim applicationName As String = Application.ProductName
+    Dim applicationPath As String = Application.ExecutablePath
+    Private Sub home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        andi.Start()
+        ' rtt.Visible = False
+        '  Label3.Visible = False
+        '  Button1.Visible = False
+        '  checks()
+        Dim regKey As Microsoft.Win32.RegistryKey
+        regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+        regKey.SetValue(applicationName, """" & applicationPath & """")
+        regKey.Close()
+
+
+    End Sub
+
+    Sub checks()
+        For Each myd In DriveInfo.GetDrives
+            If myd.IsReady Then
+                If myd.DriveType = IO.DriveType.Removable Then
+                    paths.Text = myd.Name & "Music\"
+                    ListBox1.Items.Add("Drive " & myd.Name & "  is ready")
+                        ListBox1.Items.Add("AvailableFreeSpace: " & myd.AvailableFreeSpace)
+                        ListBox1.Items.Add("DriveFormat: " & myd.DriveFormat)
+                        ListBox1.Items.Add("DriveType: " & myd.DriveType)
+                        ListBox1.Items.Add("DriveType: " & myd.DriveType.ToString)
+
+
+                End If
+                End If
+
+        Next
+    End Sub
+    Private Sub tm1_Tick(sender As Object, e As EventArgs) Handles tm1.Tick
+        Try
+            ListBox1.Items.Clear()
+            For Each myd In DriveInfo.GetDrives
+                If myd.IsReady Then
+                    If myd.DriveType = IO.DriveType.Removable Then
+                        chk = "a"
+                        If chk = "a" Then
+                            paths.Text = myd.Name & "Music\"
+                            Dim dir As New IO.DirectoryInfo(paths.Text)
+                            If dir.Exists Then
+                                'Do whatever
+
+                                ListBox1.Items.Add("Drive " & myd.Name & "  is ready")
+                                ListBox1.Items.Add("AvailableFreeSpace: " & myd.AvailableFreeSpace)
+                                ListBox1.Items.Add("DriveFormat: " & myd.DriveFormat)
+                                ListBox1.Items.Add("DriveType: " & myd.DriveType)
+                                ListBox1.Items.Add("DriveType: " & myd.DriveType.ToString)
+
+
+                                ' tm3.Start()
+
+                                keyboard_used = False
+                                mouse_used = False
+                                BlockInput(True)
+                                Threading.Thread.Sleep(1000) 'blocks for 1 second
+                                BlockInput(False)
+                                '   Call ShowDesktop()
+                                Process.Start("explorer.exe", paths.Text)
+                                tm4.Start()
+                                ' SendKeys.Send("^(a)")
+                                Me.Hide()
+
+                                ' My.Computer.Keyboard.SendKeys("Hello" + "{ENTER}")
+                                tm1.Stop()
+                            Else
+
+                            End If
+                        Else
+                                tm1.Stop()
+                                Me.Hide()
+                            End If
+
+                        End If
+
+                    End If
+
+            Next
+
+        Catch
+            End
+        End Try
+    End Sub
+    Public Sub ShowDesktop()
+        keybd_event(VK_LWIN, 0, 0, 0)
+        keybd_event(77, 0, 0, 0)
+        keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0)
+    End Sub
+
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte,
+ByVal dwFlags As Long, ByVal dwExtraInfo As Long)
+    Private Const KEYEVENTF_KEYUP = &H2
+    Private Const VK_LWIN = &H5B
+
+
+    Private Sub tm2_Tick(sender As Object, e As EventArgs) Handles tm2.Tick
+        ListBox1.Items.Clear()
+        checks()
+
+        If IO.Directory.Exists(paths.Text) Then
+
+            If chk = "a" Then
+            Else
+                andi.Start()
+                tm1.Start()
+            End If
+
+        Else
+
+            ListBox2.Items.Clear()
+            tm1.Stop()
+            tm3.Stop()
+            tm4.Stop()
+        End If
+    End Sub
+
+
+    Private Sub tm3_Tick(sender As Object, e As EventArgs) Handles tm3.Tick
+        Try
+
+            ListBox2.SelectedIndex = ListBox2.SelectedIndex + 1
+            'Dim startInfo As New ProcessStartInfo(paths.Text & ListBox2.Text)
+            ' startInfo.WindowStyle = ProcessWindowStyle.Hidden
+            Process.Start(paths.Text & ListBox2.Text)
+        Catch
+            tm3.Stop()
+        End Try
+    End Sub
+
+    Private Sub tm4_Tick(sender As Object, e As EventArgs) Handles tm4.Tick
+        Try
+            If localByName.Length > 0 Then
+                SendKeys.Send("%{F4}")
+            Else
+                SendKeys.Send("^(a)")
+                SendKeys.Send("{ENTER}")
+                SendKeys.Send("%{F4}")
+                ' Dim newProcess As Process
+
+                '   newProcess = Process.Start("explorer.exe ", "F:\Music")
+                '   newProcess.CloseMainWindow()
+                '   newProcess.Close()
+                keyboard_used = True
+                mouse_used = True
+                chk = "a"
+
+                End
+            End If
+        Catch
+            End
+        End Try
+    End Sub
+
+    Private Sub andi_Tick(sender As Object, e As EventArgs) Handles andi.Tick
+        Me.Hide()
+        andi.Stop()
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        chk = "b"
+        tm1.Start()
+        ListBox2.Items.Clear()
+        tm2.Start()
+        '   tm5.Stop()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+End Class
